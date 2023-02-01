9c4547302ba1dba8f1ed9d6a5be6ec867de22742
Leo <leo.fasano@nested.finance>
2023-02-01 10:28:13 +0100

    refactor: improve expected test values computation

--- tests/bytecode.spec.ts
+++ tests/bytecode.spec.ts
@@ -1,7 +1,7 @@
 import 'mocha';
 import { assert, expect } from 'chai';
 import { executeBytecode, uintBuffer } from './test-utils';
-import { UInt256, U256 } from '../src';
+import { UInt256, U256, toUint } from '../src';
 
 describe('Bytecode', () => {
 
@@ -33,41 +33,40 @@ describe('Bytecode', () => {
     it('sdiv', async () => {
         /* ----------------------------- CLASSIC CASES ------------------------------ */
         // Test that 8 / 2 equals 4
-        let result = (await executeBytecode('600260080560005260ff6000f3')).result
-        expect(result).to.deep.eq(uintBuffer(4, 0xff))
+        let result = (await executeBytecode('600260080560005260206000f3')).result
+        expect(result).to.deep.eq(uintBuffer(4, 0x20))
         // Test that 16 / 7 equals 2
-        result = (await executeBytecode('6007600f0560005260ff6000f3')).result
-        expect(result).to.deep.eq(uintBuffer(2, 0xff))
+        result = (await executeBytecode('6007600f0560005260206000f3')).result
+        expect(result).to.deep.eq(uintBuffer(2, 0x20))
         // Test that 10 / 3 equals 3
-        result = (await executeBytecode('6003600a0560005260ff6000f3')).result
-        expect(result).to.deep.eq(uintBuffer(3, 0xff))
+        result = (await executeBytecode('6003600a0560005260206000f3')).result
+        expect(result).to.deep.eq(uintBuffer(3, 0x20))
         // Test that 54 / -5 equals 10
-        result = (await executeBytecode('7ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffb60360560005260ff6000f3')).result
-        let uintResult = new UInt256(new Uint8Array(result.slice(0, 32).reverse()).buffer)
+        result = (await executeBytecode('7ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffb60360560005260206000f3')).result
+        let uintResult = toUint(new Uint8Array(result))
         expect(uintResult).to.deep.eq(new UInt256(10).negate())
         // Test that -16 / -7 equals 2
-        result = (await executeBytecode('7ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff97ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff00560005260ff6000f3')).result
-        expect(result).to.deep.eq(uintBuffer(2, 0xff))
+        result = (await executeBytecode('7ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff97ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff00560005260206000f3')).result
+        expect(result).to.deep.eq(uintBuffer(2, 0x20))
 
         /* ------------------------------- EDGE CASES ------------------------------- */
         // Test that -2 / -1 equals 2
-        result = (await executeBytecode('7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe0560005260ff6000f3')).result
-        expect(result).to.deep.eq(uintBuffer(2, 0xff))
+        result = (await executeBytecode('7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe0560005260206000f3')).result
+        expect(result).to.deep.eq(uintBuffer(2, 0x20))
         // Test that 2 / -1 equals -2
-        result = (await executeBytecode('7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff60020560005260ff6000f3')).result
-        uintResult = new UInt256(new Uint8Array(result.slice(0, 32).reverse()).buffer)
+        result = (await executeBytecode('7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff60020560005260206000f3')).result
+        uintResult = toUint(new Uint8Array(result))
         expect(uintResult).to.deep.eq(new UInt256(2).negate())
         // Test that -2 / 1 equals -2
-        result = (await executeBytecode('60017ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe0560005260ff6000f3')).result
-        uintResult = new UInt256(new Uint8Array(result.slice(0, 32).reverse()).buffer)
+        result = (await executeBytecode('60017ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe0560005260206000f3')).result
+        uintResult = toUint(new Uint8Array(result))
         expect(uintResult).to.deep.eq(new UInt256(2).negate())
         // Test that 2 / 1 equals 2
-        result = (await executeBytecode('600160020560005260ff6000f3')).result
-        uintResult = new UInt256(new Uint8Array(result.slice(0, 32).reverse()).buffer)
-        expect(result).to.deep.eq(uintBuffer(2, 0xff))
+        result = (await executeBytecode('600160020560005260206000f3')).result
+        expect(result).to.deep.eq(uintBuffer(2, 0x20))
         // Test that MAX_INT256 / MIN_INT256 equals -1
-        result = (await executeBytecode('7f7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7f80000000000000000000000000000000000000000000000000000000000000000560005260ff6000f3')).result
-        uintResult = new UInt256(new Uint8Array(result.slice(0, 32).reverse()).buffer)
+        result = (await executeBytecode('7f7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7f80000000000000000000000000000000000000000000000000000000000000000560005260206000f3')).result
+        uintResult = toUint(new Uint8Array(result))
         expect(uintResult).to.deep.eq(new UInt256(1).negate())
     })
 
